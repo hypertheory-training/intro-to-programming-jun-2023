2e7762e523c7d71db8c73b770e92a613eb7cb797
Jeffry Gonzalez <jeff@hypertheory.com>
2023-06-22 11:42:03 -0400

    done with that version

--- instructor/BankingSolution/Banking.UnitTests/BankAccount/GoldCustomersGetABonusOnDeposits.cs
+++ instructor/BankingSolution/Banking.UnitTests/BankAccount/GoldCustomersGetABonusOnDeposits.cs
@@ -4,9 +4,22 @@
 namespace Banking.UnitTests.BankAccount;
 public class GoldCustomersGetABonusOnDeposits
 {
-    [Fact(Skip = "Working on it")]
+    [Fact]
     public void BonusIsApplied()
     {
+        // Given - the "Context" for this test.
+        var account = new Account();
+        account.AccountType = LoyaltyLevel.Gold;
+        var openingBalance = account.GetBalance();
+        var amountToDeposit = 100M;
+        var expectedNewBalance = openingBalance + amountToDeposit + 10M;
+
+
+        // When - what we are testing
+        account.Deposit(amountToDeposit);
+
+        // Then - how do we know it worked?
+        Assert.Equal(expectedNewBalance, account.GetBalance());
 
     }
 }
--- instructor/BankingSolution/Banking.UnitTests/BankAccount/MakingDeposits.cs
+++ instructor/BankingSolution/Banking.UnitTests/BankAccount/MakingDeposits.cs
@@ -16,6 +16,8 @@ public class MakingDeposits
         // When - I do the deposit
         account.Deposit(amountToDeposit); // "Command"
 
+       
+
         // Then - I can verify it worked if the new balance is 100 more than the balance
         //        was before.
         Assert.Equal(openingBalance + amountToDeposit, account.GetBalance());
