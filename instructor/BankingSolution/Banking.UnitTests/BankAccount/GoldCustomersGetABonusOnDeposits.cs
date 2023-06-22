616bc11831175abda79e9de12518316d3c0145ac
Jeffry Gonzalez <jeff@hypertheory.com>
2023-06-22 12:06:57 -0400

    inheritence demo done

--- instructor/BankingSolution/Banking.UnitTests/BankAccount/GoldCustomersGetABonusOnDeposits.cs
+++ instructor/BankingSolution/Banking.UnitTests/BankAccount/GoldCustomersGetABonusOnDeposits.cs
@@ -1,12 +1,22 @@
 ﻿
 
 
+using Banking.Domain;
+
 namespace Banking.UnitTests.BankAccount;
 public class GoldCustomersGetABonusOnDeposits
 {
-    [Fact(Skip = "Working on it")]
+    [Fact]
     public void BonusIsApplied()
     {
 
+        var account = new GoldAccount();
+        var openingBalance = account.GetBalance();
+        var amountToDeposit = 100M;
+        var expectedNewBalance = openingBalance + amountToDeposit + 10M;
+
+        account.Deposit(amountToDeposit);
+
+        Assert.Equal(expectedNewBalance, account.GetBalance());
     }
 }
